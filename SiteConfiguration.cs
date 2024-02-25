7de4df32f000c5056dacd923d6947d7caada76a2
Sandeep Gadey <123487415+sandeepgadey@users.noreply.github.com>
2024-02-25 14:41:51 -0600

    Add project files.

---
+++ SiteConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCSALabs.com
+{
+    public static class SiteConfiguration
+    {
+        public static string StaticAssetRoot = "";
+        public static string CDNVersion = "";
+        public static string FrontDoorURL = "";
+        public static string StorageAccount = "";
+    }
+}
